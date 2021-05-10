3701da9cc1e2f16fb159680d58064c97bd0c9ecc
Oleg <olegs-97@ya.ru>
2021-05-10 14:11:55 +0300

    added file, ip address, and port selection

--- main.cpp
+++ main.cpp
@@ -5,88 +5,83 @@
 #include <time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <fcntl.h>
+#include <netdb.h> /* getprotobyname */
 
-#include <iostream>
-#include <string>
-
-int sockfd;
-
-const std::string getresponse()
-{
-    char c;
-    std::string buf;
-
-    while(1)
-    {
-        if(read(sockfd, &c, 1) != 1) break;
-        if(c == '\n') break; else buf += c;
-    }
-
-    return buf;
-}
-
-void runcommand(const std::string cmd)
-{
-    std::string buf;
-
-    std::cout<< ">> " << cmd <<std::endl;
-
-    /*
-    *
-    * Send the command to execute to the daemon.
-    *
-    */
-
-    buf = cmd + "\n";
-    write(sockfd, buf.c_str(), buf.size());
-
-    /*
-    *
-    * Now let's read the answer (one line).
-    *
-    */
-
-    std::cout<< "<< " << getresponse() <<std::endl;
-}
 
 int main(int argc, char **argv)
 {
+    char *server_reply = NULL;
+    char *user_input = NULL;
+    char buffer[BUFSIZ];
+    in_addr_t in_addr;
+    int filefd;
+    int sockfd;
+    ssize_t read_return;
+    struct hostent *hostent;
     struct sockaddr_in sin;
 
-    std::cout<< "Simple client for a TCP listening daemon, v1.0" <<std::endl <<std::endl;
-
-    memset(&sin, 0, sizeof(sin));
-
-    sin.sin_family = AF_INET;
-    sin.sin_port = htons(1667);
-    inet_pton(AF_INET, "localhost", &sin.sin_addr);
-
-    if((sockfd = socket(PF_INET, SOCK_STREAM, 0)) != -1)
+    if(argc != 4)
     {
-        std::cout<< "socket has been created successfully" <<std::endl;
-
-        if(!connect(sockfd, (struct sockaddr *) &sin, sizeof(sin)))
-        {
-            std::cout<< "connected to localhost:1667" <<std::endl;
-
-            std::cout<< "daemon says: " << getresponse() <<std::endl;
-
-            runcommand("hello");
-            runcommand("version");
-            runcommand("quit");
-
-            shutdown(sockfd, 2);
-            close(sockfd);
-
-            std::cout<< "connection terminated" <<std::endl;
-        }
-        else
-            std::cout<< "unable to connect to localhost:1667" <<std::endl;
+        printf("Write sending file, server's IP-addres and port: <file_name> <x.x.x.x> <x>");
+        return 0;
     }
-    else
-        perror("socket");
+    printf("Simple TCP client v0.0\n");
 
-    return 0;
+    filefd = open(argv[1], O_RDONLY);
+    if (filefd == -1)
+    {
+        perror("open");
+        exit(EXIT_FAILURE);
+    }
+
+    hostent = gethostbyname(argv[2]);
+    if (hostent == NULL) {
+        fprintf(stderr, "error: gethostbyname(\"%s\")\n", argv[2]);
+        exit(EXIT_FAILURE);
+    }
+
+    unsigned short server_port = strtol(argv[3], NULL, 10);
+
+    in_addr = inet_addr(inet_ntoa(*(struct in_addr*)*(hostent->h_addr_list)));
+    memset(&sin, 0, sizeof(sin));
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons(server_port);
+    sin.sin_addr.s_addr = in_addr;
+    //inet_pton(AF_INET, "localhost", &sin.sin_addr);
+
+    if((sockfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) != -1)
+    {
+        printf("Socket created\n");
+
+        if(connect(sockfd, (struct sockaddr *) &sin, sizeof(sin)) == -1)
+        {
+            perror("Connection fail\n");
+            return EXIT_FAILURE;
+        }
+        while (1)
+        {
+            read_return = read(filefd, buffer, BUFSIZ);
+            if (read_return == 0)
+                break;
+            else if (read_return == -1)
+            {
+                perror("read");
+                exit(EXIT_FAILURE);
+            }
+            
+            if (write(sockfd, buffer, read_return) == -1)
+            {
+                perror("write");
+                exit(EXIT_FAILURE);
+            }
+        }
+        free(user_input);
+        free(server_reply);
+        close(filefd);
+        exit(EXIT_SUCCESS);
+    }
 }
